a0c716749a197f4679b2cdbf59185d6353077b2b
thaismota-tech <thaissantosdamota@gmail.com>
2026-05-09 12:16:25 +0200

    He finalizado el ejercicio 3

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
 //EJERCICIO 1
 async function getAllBreeds() {
-  try {
-    const response = await fetch('https://dog.ceo/api/breeds/list/all');
-    const data = await response.json();
-    const breeds = Object.keys(data.message);  
-    return breeds;
-  } catch (error) {
-    console.error("Error al obtener las razas:", error);
-  }
+    try {
+        const response = await fetch('https://dog.ceo/api/breeds/list/all');
+        const data = await response.json();
+        const breeds = Object.keys(data.message);
+        return breeds;
+    } catch (error) {
+        console.error("Error al obtener las razas:", error);
+    }
 }
 getAllBreeds().then(razas => console.log(razas));
 
 //EJERCICIO 2
 async function getRandomDog() {
+    try {
+        const response = await fetch('https://dog.ceo/api/breeds/image/random');
+        const data = await response.json();
+        return data.message;
+    } catch (error) {
+        console.error("Error al obtener la imagen:", error);
+    }
+}
+getRandomDog().then(url => console.log("Imagen aleatoria:", url));
+
+//EJERCICIO 3
+async function getAllImagesByBreed() {
   try {
-    const response = await fetch('https://dog.ceo/api/breeds/image/random');
+    const response = await fetch('https://dog.ceo/api/breed/komondor/images');
     const data = await response.json();
     return data.message;
   } catch (error) {
-    console.error("Error al obtener la imagen:", error);
+    console.error("Error al obtener las imágenes de la raza Komondor:", error);
   }
 }
-getRandomDog().then(url => console.log("Imagen aleatoria:", url));
+getAllImagesByBreed().then(imagenes => console.log(imagenes));
+
+//EJERCICIO 4
