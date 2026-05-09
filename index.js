f06f96a5ba5dd5144cfff0ab8977877178b0e45b
thaismota-tech <thaissantosdamota@gmail.com>
2026-05-09 12:07:46 +0200

    He finalizado el ejercicio 2

--- index.js
+++ index.js
@@ -12,3 +12,13 @@ async function getAllBreeds() {
 getAllBreeds().then(razas => console.log(razas));
 
 //EJERCICIO 2
+async function getRandomDog() {
+  try {
+    const response = await fetch('https://dog.ceo/api/breeds/image/random');
+    const data = await response.json();
+    return data.message;
+  } catch (error) {
+    console.error("Error al obtener la imagen:", error);
+  }
+}
+getRandomDog().then(url => console.log("Imagen aleatoria:", url));
