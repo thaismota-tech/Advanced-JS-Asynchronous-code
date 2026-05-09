574e256204cb92362b23a5cde92ecf6c00f9e8ca
thaismota-tech <thaissantosdamota@gmail.com>
2026-05-09 11:51:13 +0200

    He finalizado el ejercicio 1

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-//RESUELVE TUS EJERCICIOS AQUI
+//EJERCICIO 1
+async function getAllBreeds() {
+  try {
+    const response = await fetch('https://dog.ceo/api/breeds/list/all');
+    const data = await response.json();
+    const breeds = Object.keys(data.message);  
+    return breeds;
+  } catch (error) {
+    console.error("Error al obtener las razas:", error);
+  }
+}
+getAllBreeds().then(razas => console.log(razas));
+
+//EJERCICIO 2
